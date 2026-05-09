f107c30ecf30b7118cf4643dbc6791d054917b9a
Steevel Salis <steevelsalis@gmail.com>
2026-05-09 19:32:36 +0530

    chore: removed dev proxy settings from vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:5000",
+    //     changeOrigin: true,
+    //   },
+    // },
   },
 });
